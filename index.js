9015ad250411531cc4f250fea41f694a6f570226
Raynos <raynos2@gmail.com>
2013-07-19 19:10:18 -0700

    support opts.name

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ function normalize(name, opts) {
         opts.placeholder = "Please enter your " + opts.label
     }
 
+    if (!opts.name) {
+        opts.name = opts.marker
+    }
+
     return opts
 }
 
@@ -46,6 +50,7 @@ function booleanCheckbox(opts) {
         ["input.input", {
             type: "checkbox",
             checked: !!opts.checked,
+            name: opts.name,
             "data-marker": "form." + opts.marker
         }],
         ["label.label", [ opts.label ]],
@@ -62,10 +67,15 @@ function tupleInput(opts) {
             JSON.stringify(opts))
     }
 
+    if (!opts.name) {
+        opts.name = opts.marker
+    }
+
     return [".tuple-input.form-elem" + (opts.selector || ""), [
         ["label.label", [ opts.label ]],
         ["input.input", {
             placeholder: opts.placeholder[0],
+            name: opts.name[0],
             "data-marker": "form." + opts.marker[0]
         }],
         [".error", {
@@ -73,6 +83,7 @@ function tupleInput(opts) {
         }],
         ["input.input", {
             placeholder: opts.placeholder[1],
+            name: opts.name[1],
             "data-marker": "form." + opts.marker[1]
         }],
         [".error", {
@@ -88,6 +99,7 @@ function stringInput(opts) {
         ["label.label", [ opts.label ]],
         ["input.input", {
             placeholder: opts.placeholder,
+            name: opts.name,
             "data-marker": "form." + opts.marker
         }],
         [".error", {
@@ -103,6 +115,7 @@ function stringPassword(opts) {
         ["label.label", [ opts.label ]],
         ["input.input", {
             placeholder: opts.placeholder,
+            name: opts.name,
             "data-marker": "form." + opts.marker,
             type: "password"
         }],
